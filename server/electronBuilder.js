cf174d7396be29d78942c4c0ec17888036428ad7
Ernie Salazar <salazare@verizon.net>
2016-11-14 13:47:15 -0500

    Better encapsulated electronBuilder code.

--- server/electronBuilder.js
+++ server/electronBuilder.js
@@ -1,6 +1,12 @@
 const packagejson = require("../package.json");
 const builder = require("electron-builder");
 
+const mimes = new Map();
+mimes.set("exe", "exe");
+mimes.set("dmg", "dmg");
+mimes.set("zip", "zip");
+mimes.set("AppImage", "x-executable");
+
 //Development package.json, see https://goo.gl/5jVxoO
 const devMetadata  = packagejson.electronBuilder;
 
@@ -13,13 +19,26 @@ const appMetadata = {
 };
 
 function buildPromise(){
-    //Build for the current target and send back promise
-    return builder.build({
-        projectDir: "./",
-        devMetadata,
-        appMetadata
-    });
+    return new Promise((resolve, reject) => {
+        builder
+            .build({ projectDir: "./", devMetadata, appMetadata })
+            .then(args => {
+                const filePath = args[0];
+                const fileName = filePath.substr(filePath.replace(/\\/g,"/").lastIndexOf("/") + 1);
+                const ext = fileName.substr(fileName.lastIndexOf(".") + 1);
 
+                let mimeType;
+                if (mimes.has(ext))
+                    mimeType = `application/${mimes.get(ext)}, application/octet-stream`;
+                else
+                    console.warn(`Unsupported file type '${ext}' in file '${filePath}'; mime type will be null`);
+                
+                resolve({fileName, filePath, mimeType});
+
+            }).catch((error) => {
+                console.error(error);
+            });
+    });
 }
 
 module.exports = buildPromise;
--- server/routes.js
+++ server/routes.js
@@ -3,12 +3,6 @@ const router = express.Router();
 const fs = require('fs');
 const electronBuilder = require('./electronBuilder');
 
-const mimes = new Map();
-mimes.set("exe", "exe");
-mimes.set("dmg", "dmg");
-mimes.set("zip", "zip");
-mimes.set("AppImage", "x-executable");
-
 //Install page for OpenFin Application
 router.get('/install', function(req, res, next){
     res.sendFile('install.html', { root: 'public' });
@@ -17,21 +11,11 @@ router.get('/install', function(req, res, next){
 //Install page for Electron Application
 router.get('/electron', function(req, res, next){
     electronBuilder()
-        .then(args => {
-            const filePath = args[0];
-            fs.readFile(filePath, (err, data) => {
-                const ext = filePath.substr(filePath.lastIndexOf(".") + 1);
-                
-                if (mimes.has(ext))
-                    res.setHeader('Content-type', `application/${mimes.get(ext)}, application/octet-stream`);
-                else
-                    console.warn(`Unsupported file type '${ext}' in file '${filePath}'; Content-Type will be omitted from response`);
-                
-                const filename = filePath.substr(filePath.replace(/\\/g,"/").lastIndexOf("/") + 1);
-                res.setHeader('Content-Disposition', `attachment; filename="${filename}"`);
-                res.send(data);
-            });
-        }).catch((error) => {
+        .then(e => fs.readFile(e.filePath, (err, data) => {
+            res.setHeader('Content-Disposition', `attachment; filename="${e.fileName}"`);
+            res.setHeader('Content-type', e.mimeType);
+            res.send(data);
+        })).catch((error) => {
             console.error(error);
         });
 });
