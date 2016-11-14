a087c62ace5a74014645a50a272522a862527857
Ernie Salazar <salazare@verizon.net>
2016-11-14 10:49:49 -0500

    Cleaned up electronBuilder.js

--- server/electronBuilder.js
+++ server/electronBuilder.js
@@ -1,20 +1,18 @@
 const packagejson = require("../package.json");
 const builder = require("electron-builder");
-const Platform = builder.Platform;
 
 //Development package.json, see https://goo.gl/5jVxoO
 const devMetadata  = packagejson.electronBuilder;
 
-function buildPromise(){
-    //Application package.json
-    const appMetadata = {
-        name: packagejson.name,
-        version: packagejson.version,
-        description: packagejson.description,
-        author: packagejson.author,
-        productName: packagejson.productName
-    };
+//Application package.json
+const appMetadata = {
+    name: packagejson.name,
+    version: packagejson.version,
+    description: packagejson.description,
+    author: packagejson.author
+};
 
+function buildPromise(){
     //Build for the current target and send back promise
     return builder.build({
         projectDir: "./",
